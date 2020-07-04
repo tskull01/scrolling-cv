6632d6f98e5f834e84fc2f0dcfbb5e30ee4ba3d3
tskull01 <tskulley29@yahoo.com>
2020-07-04 19:28:29 -0400

    :devil: in the details

--- src/components/intro/intro.tsx
+++ src/components/intro/intro.tsx
@@ -4,16 +4,39 @@ import EmailIcon from "@material-ui/icons/Email";
 import "./intro.css";
 import myPhoto from "../../images/me.jpg";
 import { gsap } from "gsap";
-
+import { fromEvent } from "rxjs";
+import Placeholder from "../../images/placeholder_2_3.png";
 class intro extends Component {
+  state = {
+    imgLoaded: false,
+  };
   imgLoaded = () => {
-    console.log("image loaded");
+    this.setState({ imgLoaded: true });
+    let placeholder = document.getElementById("placeholder");
+    if (placeholder) {
+      placeholder.style.display = "none";
+    }
     gsap.to(".intro-photo", {
       opacity: 1,
       duration: 0.75,
     });
   };
+  imgLoadEvent = () => {
+    let imgRef = document.getElementsByClassName("intro-photo")[0];
+    if (imgRef) {
+      let imgSub = fromEvent(imgRef, "load").subscribe((loaded: any) => {
+        this.imgLoaded();
+      });
+    } else {
+      console.log("image ref null");
+    }
+  };
   render() {
+    {
+      let classes = this.state.imgLoaded
+        ? "intro-photo"
+        : "intro-photo hide-photo ";
+    }
     return (
       <div className="intro-container">
         <div className="intro-text">
@@ -26,7 +49,28 @@ class intro extends Component {
           </div>
         </div>
         <div className="intro-grid">
-          <img src={myPhoto} onLoad={this.imgLoaded} className="intro-photo" />
+          {this.state.imgLoaded ? (
+            <img
+              src={myPhoto}
+              onLoad={this.imgLoaded}
+              className="intro-photo "
+            />
+          ) : (
+            <div>
+              <img
+                src={myPhoto}
+                onLoad={this.imgLoaded}
+                className="intro-photo "
+                style={{ display: "none" }}
+              />
+              <img
+                src={Placeholder}
+                onLoad={this.imgLoadEvent}
+                id="placeholder"
+              />
+            </div>
+          )}
+
           <div className="icon-adjust">
             <div className="git-flex">
               <GitHubIcon />
